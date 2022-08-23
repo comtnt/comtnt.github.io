4b3b20b7c587f425c4a32fb0a4555b6511994a73
ComTNT <git@comtnt.io>
2022-08-23 14:20:19 +0800

    api

--- source/api/date.py
+++ source/api/date.py
@@ -4,7 +4,6 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
